33ee76c3d1ff8c55b5b40eda3ea83f93b3bfd62e
gilmujjang <alsanrlf@naver.com>
2022-02-07 21:05:46 +0900

    feat: 필터 컨테이너 마크업

---
+++ src/commons/common.ts
@@ -0,0 +1,10 @@
+export const MATERIAL = [
+  '구리',
+  '알류미늄',
+  '탄소강',
+  '강철',
+  '스테인리스강',
+  '합금강',
+];
+
+export const PROCESSING_METHOD = ['밀링', '선반'];
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,21 +1,79 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, MouseEventHandler } from 'react';
 import { OrderInfo } from '../../commons/type';
 import getApi from '../../commons/utils';
+import { MATERIAL, PROCESSING_METHOD } from '../../commons/common';
 
 const Container: React.FC = () => {
   const [state, setState] = useState<OrderInfo[]>([]);
+  const [filteredState, setFilteredState] = useState<OrderInfo[]>([]);
+  const [filterCondition, setFilterCondition] = useState<string[]>([]);
+  const [isMaterialActive, setIsMaterialActive] = useState(false);
+  const [isProcessingActive, setIsProcessingActive] = useState(false);
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.currentTarget.name;
+    if (target === 'material') {
+      setIsMaterialActive(!isMaterialActive);
+    } else {
+      setIsProcessingActive(!isProcessingActive);
+    }
+  };
 
   useEffect(() => {
     async function GetApi() {
       const data = await getApi('https://sixted-mock-server.herokuapp.com/');
       setState(data);
+      setFilteredState(data);
+      setFilterCondition([]);
     }
     GetApi();
   }, []);
+  console.log(state, filteredState, filterCondition);
 
-  console.log(state);
-
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h2>들어온 요청</h2>
+        <span>파트너님에게 딱 맞는 요청서를 찾아보세요.</span>
+      </div>
+      <div>
+        <div>
+          <button name="material" onClick={onClick}>
+            재료
+          </button>
+          {isMaterialActive && (
+            <ul>
+              {MATERIAL.map((material, index) => (
+                <li key={index}>
+                  <input type="checkbox"></input>
+                  {material}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div>
+          <button name="processingMethod" onClick={onClick}>
+            가공방식
+          </button>
+          {isProcessingActive && (
+            <ul>
+              {PROCESSING_METHOD.map((method, index) => (
+                <li key={index}>
+                  <input type="checkbox"></input>
+                  {method}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+      <div>
+        <span>토글</span>
+        <span>상담 중인 요청만 보기</span>
+      </div>
+    </div>
+  );
 };
 
 export default Container;
